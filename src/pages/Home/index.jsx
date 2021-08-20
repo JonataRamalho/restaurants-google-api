324a0a98fd9d9b04cde40db64b782506e5f876e3
Jonata Ramalho <jonataramalho.s@gmail.com>
2021-08-19 21:43:56 -0300

    feat: adiciona logo

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import logo from '../../assets/logo.svg';
+
+import { Container, Search } from './styles';
+
+const Home = () => (
+  <Container>
+    <Search>
+      <img src={logo} alt="Logo do restaurante" />
+    </Search>
+  </Container>
+);
+
+export default Home;
