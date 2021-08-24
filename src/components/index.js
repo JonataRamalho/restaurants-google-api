335ea0a7951201664f3972ff7020f2713c935b1c
Jonata Ramalho <jonataramalho.s@gmail.com>
2021-08-24 18:27:56 -0300

    feat: adiciona index para organizar os imports

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Card } from './ImageCard';
+
+// Index.js nos componentes serve para organizar os imports no app.jsx ou home.jsx
+// Não ira precisar criar vários imports para um determinado arquivo.
