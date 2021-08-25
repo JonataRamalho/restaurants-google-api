77ba4fe9d22e6c34be2613841e0d7d8454099c45
Jonata Ramalho <jonataramalho.s@gmail.com>
2021-08-24 23:27:32 -0300

    feat: cria componente para apresentar resturantes

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 export { default as Card } from './ImageCard';
+export { default as RestaurantCard } from './RestaurantCard';
 
 // Index.js nos componentes serve para organizar os imports no app.jsx ou home.jsx
 // Não ira precisar criar vários imports para um determinado arquivo.
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,7 +5,7 @@ import MaterialIcon from '@material/react-material-icon';
 
 import logo from '../../assets/logo.svg';
 import restaurante from '../../assets/restaurante-fake.png';
-import { Card } from '../../components';
+import { Card, RestaurantCard } from '../../components';
 
 import { Container, Carousel, Search, Logo, Wrapper, Map, CarouselTitle } from './styles';
 
@@ -44,6 +44,7 @@ const Home = () => {
             <Card photo={restaurante} title="Teste teste" />
           </Carousel>
         </Search>
+        <RestaurantCard />
       </Container>
       <Map />
     </Wrapper>
