e1c19b85a7000d8a36a36f1d22a078717f869b6e
Jonata Ramalho <jonataramalho.s@gmail.com>
2021-08-25 19:02:37 -0300

    feat: cria componente modal

---
+++ src/components/Modal/Portal/index.jsx
@@ -0,0 +1,10 @@
+import { ReactDOM } from 'react-dom';
+
+const PortalModal = ({ children }) => {
+  const portal = document.getElementById('modal-root');
+  return ReactDOM.createPortal(children, portal);
+};
+
+export default PortalModal;
+
+// Parou Dev de componentes - Parte 4
