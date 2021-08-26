d0b3cc500ee53b3f1eb306dde60bc1908fc49378
Jonata Ramalho <jonataramalho.s@gmail.com>
2021-08-25 22:26:51 -0300

    feat: adiciona modal em home

--- src/components/Modal/Portal/index.jsx
+++ src/components/Modal/Portal/index.jsx
@@ -1,4 +1,4 @@
-import { ReactDOM } from 'react-dom';
+import ReactDOM from 'react-dom';
 
 const PortalModal = ({ children }) => {
   const portal = document.getElementById('modal-root');
--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 export { default as Card } from './ImageCard';
 export { default as RestaurantCard } from './RestaurantCard';
+export { default as Modal } from './Modal';
 
 // Index.js nos componentes serve para organizar os imports no app.jsx ou home.jsx
 // Não ira precisar criar vários imports para um determinado arquivo.
