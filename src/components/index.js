561594d9b438da51e7f0559a98c72883bdfc5648
JonataRamalho <jonataramalho.s@gmail.com>
2021-09-09 11:48:03 -0300

    feat: adiciona skeleton

--- src/components/index.js
+++ src/components/index.js
@@ -3,6 +3,7 @@ export { default as RestaurantCard } from './RestaurantCard';
 export { default as Modal } from './Modal';
 export { default as Map } from './Map';
 export { default as Loader } from './Loader';
+export { default as Skeleton } from './Skeleton';
 
 // Index.js nos componentes serve para organizar os imports no app.jsx ou home.jsx
 // Não ira precisar criar vários imports para um determinado arquivo.
